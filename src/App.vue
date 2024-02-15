36f1cb5e7439cba1626f9e9b4c88a3b1a5a2fc47
yoosonchan <yoosonchan@163.com>
2024-02-15 20:03:01 +0800

    feat: update input component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref, unref } from 'vue';
 const isbn = ref('')
 const inputRef = ref()
-// const result = ref('content')
+const result = ref('')
+// listener
+const handleKeydown = (e: KeyboardEvent) => {
+  if (e.code === 'Enter') {
+    result.value = unref(isbn)
+    // clear input content
+    isbn.value = ''
+  }
+}
+const addListener = () => {
+  document.addEventListener('keydown', handleKeydown, false)
+}
+const removeListener = () => {
+  document.removeEventListener('keydown', handleKeydown)
+}
+// lifecycle
 onMounted(() => {
   inputRef.value.focus()
+  addListener()
+})
+onUnmounted(() => {
+  removeListener()
 })
 </script>
 
 <template>
   <div class="flex flex-col items-center mt-[6%]">
-    <div class="mb-3 text-lg font-bold">Hello Douban Book</div>
+    <div class="mb-3 text-lg font-bold">Book</div>
     <div class="w-72">
-      <a-input v-model="isbn" size="large" :ref="inputRef"></a-input>
+      <a-input v-model="isbn" ref="inputRef" size="large">
+        <template #prefix>ISBN:</template>
+      </a-input>
     </div>
-    <div class="mt-5">{{ isbn }}</div>
+    <div class="mt-5">{{ result }}</div>
   </div>
 </template>
