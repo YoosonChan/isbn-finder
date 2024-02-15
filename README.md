7bae6763ce346ec700af9e73a86753c8086aedc3
yoosonchan <yoosonchan@163.com>
2024-02-15 19:14:53 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # douban-book
+
 douban book search by ISBN
+
+## TODO
+
+- [] writing query methods
+- [] displaying query results
+- [] storing query results
+- [] updating data to vika
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+const isbn = ref('')
+const inputRef = ref()
+// const result = ref('content')
+onMounted(() => {
+  inputRef.value.focus()
+})
+</script>
+
+<template>
+  <div class="flex flex-col items-center mt-[6%]">
+    <div class="mb-3 text-lg font-bold">Hello Douban Book</div>
+    <div class="w-72">
+      <a-input v-model="isbn" size="large" :ref="inputRef"></a-input>
+    </div>
+    <div class="mt-5">{{ isbn }}</div>
+  </div>
+</template>
